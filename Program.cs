56e14711095640a402db44660d4356bd4d112789
PROPS1 <70951327+PROPS1@users.noreply.github.com>
2020-09-22 14:59:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace practica4
+{
+  class Program
+  {
+
+    static void Main(string[] args)
+    {
+      ArrayList myList = new ArrayList() { "first" };
+      myList.Add("second");
+      myList.Add("third");
+      myList.Add("fourth");
+      foreach (string item in myList)
+      {
+        Console.WriteLine("Unsorted: {0}", item);
+      }
+      myList.Sort();
+      foreach (string item in myList)
+      {
+        Console.WriteLine("   Sorted: {0}", item);
+      }
+
+    }
+
+  }
+}
