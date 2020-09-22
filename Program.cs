b89bffdbb79047e3e3bfee9fc791da9763375db6
PROPS1 <70951327+PROPS1@users.noreply.github.com>
2020-09-22 15:21:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Globalization;
+
+namespace practica4
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      ListDictionary list = new ListDictionary(new CaseInsensitiveComparer(CultureInfo.InvariantCulture));
+      list["Estados Unidos"] = "United States of America";
+      list["Canada"] = "Canada";
+      list["Espana"] = "Spain";
+      Console.WriteLine(list["espana"]);
+      Console.WriteLine(list["CANADA"]);
+      Console.Read();
+    }
+
+  }
+
+}
