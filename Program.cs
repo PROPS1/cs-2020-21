91e9f9c49987eeed79c793bcb7f8ca578f3b958d
PROPS1 <70951327+PROPS1@users.noreply.github.com>
2020-09-22 15:17:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace practica4
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Hashtable lookup = new Hashtable();
+
+      lookup["0"] = "zero";
+      lookup["1"] = "one";
+      lookup["2"] = "two";
+      lookup["3"] = "three";
+      lookup["4"] = "four";
+      lookup["5"] = "five";
+      lookup["6"] = "six";
+      lookup["7"] = "seven";
+      lookup["8"] = "eight";
+      lookup["9"] = "nine";
+
+      string ourNumber = "888-555-1212";
+      foreach (char c in ourNumber)
+      {
+        string digit = c.ToString();
+        if (lookup.ContainsKey(digit))
+        {
+          Console.WriteLine(lookup[digit]);
+        }
+      }
+
+    }
+  }
+}
